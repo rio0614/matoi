fee63f34d596ea833a5d624d43540708f7d83520
Ryosuke Kimura <rio@RyosukenoMacBook-Air.local>
2022-01-26 02:00:31 +0900

    add jQuery description

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ $(function () {
         $header = $('header, header a, header .headerLogo a, .nav_toggle i');
     const $headerOffsetTop = $('body').offset().top;
 
-// ナビゲーションをクリックした時のスムーズスクロール
+// トップへ戻るボタンのスムーズスクロール
     $('a[href^="#"]').click(function(){
         let href= $(this).attr("href");
         let target = $(href == "#" || href == "" ? 'html' : href);
